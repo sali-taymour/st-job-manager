ef4391cd95cc17affb0f7b523c83fe3c7f0a2701
sali <sali.taymor@gmail.com>
2022-07-12 16:20:32 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.scss";
 import axios from "axios";
 
 const backend_url = import.meta.env.VITE_BACKEND_URL;
-
+console.log(backend_url);
 function App() {
     const [jobSources, setJobSources] = useState([]);
 
@@ -15,7 +15,7 @@ function App() {
 
     return (
         <div className="App">
-            <h1>EJ2 Job Manager</h1>
+            <h1>St Job Manager</h1>
             <p>There are {jobSources.length} job sources:</p>
             <ul>
                 {jobSources.map((jobSource, i) => {
